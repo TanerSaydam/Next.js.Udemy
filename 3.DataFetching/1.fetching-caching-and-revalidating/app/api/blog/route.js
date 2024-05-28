b9837ebcf9e162770ad99e90715721feb8114245
TanerSaydam <tanersaydam@gmail.com>
2024-05-29 00:38:11 +0300

    DataFetching - Revalidating Data

--- 3.DataFetching/1.fetching-caching-and-revalidating/app/api/blog/route.js
+++ 3.DataFetching/1.fetching-caching-and-revalidating/app/api/blog/route.js
@@ -1,5 +1,7 @@
+export const revalidate = 3600;
+
 export async function GET(){
-    const res = await fetch("endpoint", {cache: "force-cache"});
+    const res = await fetch("endpoint", {cache: "force-cache", next: {revalidate: 3600, tags: ['collection']}});
 
     if(!res.ok){
         throw new Error("Failed to fetch data")
@@ -7,3 +9,4 @@ export async function GET(){
 
     return Response.json(res.json());
 }
+
