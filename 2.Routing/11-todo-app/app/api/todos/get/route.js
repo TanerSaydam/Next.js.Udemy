0135d77e31b49755d3ec646f2666949f25fb03f6
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 15:36:30 +0300

    Routing - Revalidating Cached Data

--- 2.Routing/11-todo-app/app/api/todos/get/route.js
+++ 2.Routing/11-todo-app/app/api/todos/get/route.js
@@ -1,6 +1,9 @@
+export const revalidate = 1;
+
 export async function GET(){
     const result = await fetch("http://localhost:9500/todos",{
-        cache: "no-store"
+        cache: "no-cache"
+       //next: { revalidate: 1}
     }).then(res=> res.json());
     return Response.json(result);
 }
