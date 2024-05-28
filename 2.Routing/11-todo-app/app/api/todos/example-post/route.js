ca068344aa35f7a68897c3440b1bf496def3cdd2
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 16:03:07 +0300

    Routing - Request Body FormData

--- 2.Routing/11-todo-app/app/api/todos/example-post/route.js
+++ 2.Routing/11-todo-app/app/api/todos/example-post/route.js
@@ -1,5 +1,9 @@
 export async function POST(request){
-    const res = await request.json();
+    //const res = await request.json();
+    const formData = await request.formData();
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const age = formData.get("age");
 
-    return Response.json(res);
+    return Response.json({name: name, email: email, age: age});
 }
