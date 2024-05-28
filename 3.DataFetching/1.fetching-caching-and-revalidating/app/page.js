0516ad58b2a9a852196985e67773b9e476cfcd64
TanerSaydam <tanersaydam@gmail.com>
2024-05-29 00:21:41 +0300

    DataFetching - Data Fetching, Caching, and Revalidating

---
+++ 3.DataFetching/1.fetching-caching-and-revalidating/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Fetching, Caching, and Revalidating</h1>
+
+      <ul>
+        <li>On the server, with fetch</li>
+        <li>On the server, with third-party libraries</li>
+        <li>On the client, via a Route Handler</li>
+        <li>On the client, with third-party libraries</li>
+      </ul>
+    </main>
+  );
+}
