85c5aeee0eb2d8489f4c605a97895c8f585f4438
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 17:19:20 +0300

    Routing - Conditional Statements

---
+++ 2.Routing/12-middleware/app/log/page.js
@@ -0,0 +1,3 @@
+export default function LogPage(){
+    return <h1>Log Page</h1>
+}
--- 2.Routing/12-middleware/middleware.js
+++ 2.Routing/12-middleware/middleware.js
@@ -1,4 +1,13 @@
+import { NextResponse } from "next/server";
+
 export function middleware(request){
     console.log("Middleware is working...");
-    console.log(request);
+    //console.log(request);
+    if(!request.nextUrl.pathname.includes("/login")){
+        return NextResponse.redirect(new URL("/auth/login", request.url))
+    }
 }
+
+// export const config = {
+//     matcher: ['/log/:path*',"/"]
+// }
