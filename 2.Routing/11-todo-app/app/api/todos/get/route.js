5a57e147dbcf68eec6ad248d3a5c2029162066d8
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 15:49:40 +0300

    Routing - Dynamic Functions

--- 2.Routing/11-todo-app/app/api/todos/get/route.js
+++ 2.Routing/11-todo-app/app/api/todos/get/route.js
@@ -1,9 +1,20 @@
+import { cookies, headers } from 'next/headers'
+
 export const revalidate = 1;
 
 export async function GET(){
+    const cookieStore = cookies();
+    const token = cookieStore.get("token");
+    const headersList = headers();
+    const secretKey = headersList.get("secretKey");
+
     const result = await fetch("http://localhost:9500/todos",{
         cache: "no-cache"
        //next: { revalidate: 1}
     }).then(res=> res.json());
-    return Response.json(result);
+
+    return new Response(secretKey, {
+        status: 200,
+        headers: { 'Set-Cookie': 'token=' + token}
+    });
 }
