b1b0b66cb902c4463f603033cafc8f6de7a50c7d
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 17:45:11 +0300

    Routing - Setting Headers

--- 2.Routing/12-middleware/middleware.js
+++ 2.Routing/12-middleware/middleware.js
@@ -1,23 +1,17 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request){
-    console.log("Middleware is working...");
-    const requestCookie = request.cookies.get("token");
-   
-    const allCookies = request.cookies.getAll();
-    //console.log(allCookies);
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-hello-from-middleware", "hello");
 
-    request.cookies.has('token'); // true / false
-    request.cookies.delete("token2");
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeaders
+        }
+    });
 
-    const response = NextResponse.next();
-    response.cookies.set("token","my token")
-    const responseCookie = response.cookies.get("token");
-
-    console.log(requestCookie);
-    console.log(responseCookie);
-
-    //allCookies = response.cookies.getAll();
-    //console.log(allCookies);
-    
+    response.headers.set("x-hello-from-middleware", "hello");
+    response.headers.set("authorization", "bearer token");
+    response.cookies.set("token","mymytoken");
+    return response;
 }
