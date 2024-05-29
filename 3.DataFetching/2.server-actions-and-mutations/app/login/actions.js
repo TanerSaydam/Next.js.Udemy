6e8069faf5f11f1b8bbf030c94e2b607490fe280
TanerSaydam <tanersaydam@gmail.com>
2024-05-29 19:58:19 +0300

    DataFetching - Manual validation (without zod)

--- 3.DataFetching/2.server-actions-and-mutations/app/login/actions.js
+++ 3.DataFetching/2.server-actions-and-mutations/app/login/actions.js
@@ -1,26 +1,16 @@
 'use server'
 
 import { redirect } from 'next/navigation';
-import {z} from 'zod';
 
-const scheme = z.object({
-    email: z.number({
-        invalid_type_error: "Invalid email"
-    })
-})
-
-export async function login(prevState, formData){
-    const validatedFields = scheme.safeParse({
-        email: +formData.get("email")
-    });
-
-    if(!validatedFields.success){
-        return {
-            message: JSON.stringify(validatedFields.error.flatten().fieldErrors)
-        }
-    }
+export async function login(prevState, formData){    
 
     const email = formData.get("email");
+
+    if(email.length < 15){
+        return {
+            message: "Invalid email!!"
+        }
+    }    
 
     redirect("/login");
 }
